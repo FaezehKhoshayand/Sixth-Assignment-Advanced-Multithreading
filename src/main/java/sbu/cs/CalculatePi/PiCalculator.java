05701f9d61894273defe227de967a1f748ac175d
FaezehKhoshayand <faezehkhoshayand@gmail.com>
2024-05-24 23:57:25 +0330

    calculateHard2 test case is not passed

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -1,4 +1,12 @@
+
 package sbu.cs.CalculatePi;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class PiCalculator {
 
@@ -15,13 +23,51 @@ public class PiCalculator {
      * @return pi in string format (the string representation of the BigDecimal object)
      */
 
-    public String calculate(int floatingPoint)
-    {
+    public static class calculateTask implements Runnable {
+        MathContext mc = new MathContext(1000);
+        BigDecimal value;
+        private int k;
+        public calculateTask(int k){
+            this.k = k;
+        }
+        @Override
+        public void run() {
+            BigDecimal c1 = new BigDecimal(1).divide(new BigDecimal(16).pow(k),mc);
+            BigDecimal c2 = new BigDecimal(4).divide(new BigDecimal(8*k+1),mc);
+            BigDecimal c3 = new BigDecimal(-2).divide(new BigDecimal(8*k+4),mc);
+            BigDecimal c4 = new BigDecimal(-1).divide(new BigDecimal(8*k+5),mc);
+            BigDecimal c5 = new BigDecimal(-1).divide(new BigDecimal(8*k+6),mc);
+            value = c1.multiply((c2.add(c3).add(c4).add(c5)),mc);
+            addToSum(value);
+        }
+    }
+    public static BigDecimal pi;
+
+    public static synchronized void addToSum(BigDecimal value) {
+        pi = pi.add(value);
+    }
+
+    public String calculate (int floatingPoint) {
         // TODO
-        return null;
+        pi = new BigDecimal(0);
+        ExecutorService threadPool = Executors.newFixedThreadPool(4);  //a power of 2
+
+        for (int i = 0; i <= 1000; i++){     // increasing the number of iterations improves accuracy
+            calculateTask task = new calculateTask(i);
+            threadPool.execute(task);
+        }
+        threadPool.shutdown();
+        try {
+            threadPool.awaitTermination(10000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        pi = pi.setScale(floatingPoint, RoundingMode.DOWN);
+        return pi.toString();
     }
 
     public static void main(String[] args) {
         // Use the main function to test the code yourself
+
     }
 }
